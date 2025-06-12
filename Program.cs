b1973fa2b5f3029e5b0247c4136a2020f3262bed
roshan-singh2020 <92023811+roshan-singh2020@users.noreply.github.com>
2025-06-12 21:57:04 +0545

    Delete Program.cs

--- Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-var app = builder.Build();
-app.UseRouting();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
